1bafb80847f6c81d189c30cfa582c0f2200cacec
Mark Polosukhin <markipulooo@gmail.com>
2022-12-01 15:44:31 +0200

    add header

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,5 @@
 import * as flsFunctions from './modules/functions.js'
+import './modules/dropdown.js'
 
 flsFunctions.isWebp()
 
-/*
-import Swiper, { Navigation, Pagination } from 'swiper';
-const swiper = new Swiper();
-*/

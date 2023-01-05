12fea501ebbe9e28ba374c06315188108aa42a98
Mark Polosukhin <markipulooo@gmail.com>
2023-01-05 21:47:10 +0200

    add reviews section

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,38 @@
 import * as flsFunctions from './modules/functions.js'
 import '../../node_modules/bootstrap/js/src/collapse.js'
 import '../../node_modules/bootstrap/js/src/dropdown.js'
+import '../../node_modules/bootstrap/js/src/carousel.js'
+import $ from 'jquery'
+import '../../node_modules/slick-carousel/slick/slick.js'
+
+$('.rev-slider').slick({
+  infinite: true,
+  speed: 300,
+  slidesToShow: 3,
+  slidesToScroll: 3,
+  adaptiveHeight: true,
+  responsive: [
+    {
+      breakpoint: 1400,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+      },
+    },
+    {
+      breakpoint: 992,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      },
+    },
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        dots: true,
+      },
+    },
+  ],
+})

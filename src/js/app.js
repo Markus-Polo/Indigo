f81218446cec6bc4b6e86fccfc279cf66628d5a9
Mark Polosukhin <markipulooo@gmail.com>
2023-01-06 18:42:03 +0200

    fix

--- src/js/app.js
+++ src/js/app.js
@@ -1,36 +1,20 @@
 import * as flsFunctions from './modules/functions.js'
+import $ from 'jquery'
 import '../../node_modules/bootstrap/js/src/collapse.js'
 import '../../node_modules/bootstrap/js/src/dropdown.js'
-import '../../node_modules/bootstrap/js/src/carousel.js'
-import $ from 'jquery'
 import '../../node_modules/slick-carousel/slick/slick.js'
+import './modules/rev-slider.js'
 
 $('.rev-slider').slick({
   infinite: true,
   speed: 300,
-  slidesToShow: 3,
-  slidesToScroll: 3,
+  slidesToShow: 1,
+  slidesToScroll: 1,
   adaptiveHeight: true,
   responsive: [
     {
-      breakpoint: 1400,
-      settings: {
-        slidesToShow: 2,
-        slidesToScroll: 2,
-      },
-    },
-    {
-      breakpoint: 992,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1,
-      },
-    },
-    {
       breakpoint: 768,
       settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1,
         dots: true,
       },
     },

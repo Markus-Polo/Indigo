2b726096099c8f6448cb571549125a2813b02ae7
Mark Polosukhin <markipulooo@gmail.com>
2023-01-07 17:53:41 +0200

    fix again

--- src/js/app.js
+++ src/js/app.js
@@ -1,22 +1,5 @@
 import * as flsFunctions from './modules/functions.js'
-import $ from 'jquery'
 import '../../node_modules/bootstrap/js/src/collapse.js'
 import '../../node_modules/bootstrap/js/src/dropdown.js'
 import '../../node_modules/slick-carousel/slick/slick.js'
 import './modules/rev-slider.js'
-
-$('.rev-slider').slick({
-  infinite: true,
-  speed: 300,
-  slidesToShow: 1,
-  slidesToScroll: 1,
-  adaptiveHeight: true,
-  responsive: [
-    {
-      breakpoint: 768,
-      settings: {
-        dots: true,
-      },
-    },
-  ],
-})

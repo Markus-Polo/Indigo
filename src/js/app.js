7db11ce82793fdfeba6a6f811e0133874ef6dab8
Mark Polosukhin <markipulooo@gmail.com>
2023-01-15 21:23:04 +0200

    add modal

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,7 @@
 import * as flsFunctions from './modules/functions.js'
 import '../../node_modules/bootstrap/js/src/collapse.js'
 import '../../node_modules/bootstrap/js/src/dropdown.js'
+import '../../node_modules/bootstrap/js/src/modal.js'
 import '../../node_modules/slick-carousel/slick/slick.js'
 import './modules/rev-slider.js'
 import './modules/price-slider.js'
